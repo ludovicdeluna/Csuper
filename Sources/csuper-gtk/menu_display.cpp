187d195f322e03c68473e803c99cb782bb255772
Rémi BERTHO <remi.bertho@openmailbox.org>
2015-08-11 17:29:04 +0200

    Update menu display

--- Sources/csuper-gtk/menu_display.cpp
+++ Sources/csuper-gtk/menu_display.cpp
@@ -37,23 +37,88 @@
 
 using namespace Gtk;
 using namespace Glib;
+using namespace csuper;
 
 MenuDisplay::MenuDisplay(BaseObjectType* cobject, const RefPtr<Builder>& refGlade) :  CsuWidget(), Menu(cobject)
 {
+    // Glade
     refGlade->get_widget("menu_display_podium", podium_);
     refGlade->get_widget("menu_display_chart_total_points", chart_total_points_);
     refGlade->get_widget("menu_display_chart_points", chart_points_);
     refGlade->get_widget("menu_display_statistics", statistics_);
+
     refGlade->get_widget("menuitem_separator_1", sep_1_);
+
     refGlade->get_widget("menu_display_consecutive", cons_);
     refGlade->get_widget("menu_display_first", first_);
     refGlade->get_widget("menu_display_last", last_);
+
     refGlade->get_widget("menuitem_separator_2", sep_2_);
+
     refGlade->get_widget("menu_display_totalpoints", total_points_);
     refGlade->get_widget("menu_display_ranking", ranking_);
     refGlade->get_widget("menu_display_edit_suppr", edit_suppr_);
+
     refGlade->get_widget("menuitem_separator3", sep_3_);
+
     refGlade->get_widget("menu_display_ranking_side", ranking_side_);
     refGlade->get_widget("menu_display_calculator", calculator_);
     refGlade->get_widget("menu_display_game_information", game_information_);
+
+
+    // Set from pref
+    cons_->set_active(app()->pref()->differenceBetweenPlayer().consecutive());
+    first_->set_active(app()->pref()->differenceBetweenPlayer().first());
+    last_->set_active(app()->pref()->differenceBetweenPlayer().last());
+
+    total_points_->set_active(app()->pref()->scoreDisplay().totalPoints());
+    ranking_->set_active(app()->pref()->scoreDisplay().ranking());
+    edit_suppr_->set_active(app()->pref()->scoreDisplay().editSuppr());
+
+    ranking_side_->set_active(app()->pref()->mainWindowDisplay().ranking());
+    calculator_->set_active(app()->pref()->mainWindowDisplay().calculator());
+    game_information_->set_active(app()->pref()->mainWindowDisplay().gameInformation());
+
+
+    // Connect signal
+    cons_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::diffChanged));
+    first_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::diffChanged));
+    last_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::diffChanged));
+
+    total_points_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::scoreDisplayChanged));
+    ranking_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::scoreDisplayChanged));
+    edit_suppr_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::scoreDisplayChanged));
+
+    ranking_side_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::mainWindowDisplayChanged));
+    calculator_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::mainWindowDisplayChanged));
+    game_information_->signal_toggled().connect(mem_fun(*this,&MenuDisplay::mainWindowDisplayChanged));
+}
+
+
+//
+// Function
+//
+void MenuDisplay::diffChanged()
+{
+    app()->pref()->differenceBetweenPlayer().setConsecutive(cons_->get_active());
+    app()->pref()->differenceBetweenPlayer().setFirst(first_->get_active());
+    app()->pref()->differenceBetweenPlayer().setLast(last_->get_active());
+    app()->pref()->writeToFile();
+}
+
+
+void MenuDisplay::scoreDisplayChanged()
+{
+    app()->pref()->scoreDisplay().setTotalPoints(total_points_->get_active());
+    app()->pref()->scoreDisplay().setRanking(ranking_->get_active());
+    app()->pref()->scoreDisplay().setEditSuppr(edit_suppr_->get_active());
+    app()->pref()->writeToFile();
+}
+
+void MenuDisplay::mainWindowDisplayChanged()
+{
+    app()->pref()->mainWindowDisplay().setRanking(ranking_side_->get_active());
+    app()->pref()->mainWindowDisplay().setCalculator(calculator_->get_active());
+    app()->pref()->mainWindowDisplay().setGameInformation(game_information_->get_active());
+    app()->pref()->writeToFile();
 }
--- Sources/csuper-gtk/menu_display.h
+++ Sources/csuper-gtk/menu_display.h
@@ -47,15 +47,21 @@ protected:
     Gtk::MenuItem* chart_total_points_;     /*!< The chart total points button */
     Gtk::MenuItem* chart_points_;           /*!< The chart points button */
     Gtk::MenuItem* statistics_;             /*!< The statistics button */
+
     Gtk::SeparatorMenuItem* sep_1_;         /*!< The separator 1 */
+
     Gtk::CheckMenuItem* cons_;              /*!< The consecutive button */
     Gtk::CheckMenuItem* first_;             /*!< The first button */
     Gtk::CheckMenuItem* last_;              /*!< The last button */
+
     Gtk::SeparatorMenuItem* sep_2_;         /*!< The separator 2 */
+
     Gtk::CheckMenuItem* total_points_;      /*!< The total points button */
     Gtk::CheckMenuItem* ranking_;           /*!< The ranking button */
     Gtk::CheckMenuItem* edit_suppr_;        /*!< The edit suppr button */
+
     Gtk::SeparatorMenuItem* sep_3_;         /*!< The separator 3 */
+
     Gtk::CheckMenuItem* ranking_side_;      /*!< The ranking side button */
     Gtk::CheckMenuItem* calculator_;        /*!< The calculator button */
     Gtk::CheckMenuItem* game_information_;  /*!< The game information button */
@@ -70,6 +76,26 @@ public:
      *  \param refGlade the builder
      */
     MenuDisplay(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);
+
+
+
+    //
+    // Function
+    //
+    /*!
+     *  \brief Slot used when a difference between player is changed
+     */
+    void diffChanged();
+
+    /*!
+     *  \brief Slot used when a score display is changed
+     */
+    void scoreDisplayChanged();
+
+    /*!
+     *  \brief Slot used when a main window display is changed
+     */
+    void mainWindowDisplayChanged();
 };
 
 #endif // MENU_DISPLAY_H_INCLUDED
